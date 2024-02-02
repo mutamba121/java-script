4a3683142c51ea03202fa6326151f0005f9eade4
mutamba121 <joaofernandes.s121@gmail.com>
2024-02-02 17:48:21 -0300

    alteracões

---
+++ atividade login/script.js
@@ -0,0 +1,5 @@
+function entrar() {
+    // Obter os valores dos campos de entrada
+    const email =document.getElementById("e-mail").value;
+    const senha = document.getElementById("senha").value; 
+}
