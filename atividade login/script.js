8181dac8907bfe8b7ab66300bd5ec4d35aae805d
mutamba121 <joaofernandes.s121@gmail.com>
2024-02-02 17:59:26 -0300

    correção

--- atividade login/script.js
+++ atividade login/script.js
@@ -3,3 +3,9 @@ function entrar() {
     const email =document.getElementById("e-mail").value;
     const senha = document.getElementById("senha").value; 
 }
+ 
+if (email === "aluno222@aluno.senai.br" && senha === "senai.br") {
+    alert("login bem-sucedido!");
+} else {
+    alert("crendeciais onválidas.tente novamente.");
+}
