370111347a061e403f1c6c295c070f9f59b0a8e1
mutamba121 <joaofernandes.s121@gmail.com>
2024-02-07 16:46:06 -0300

    açougueiro

---
+++ atividade açougue/script.js
@@ -0,0 +1,24 @@
+function calcularTotal(a, b, c, d) {
+    return (a + b + c + d);
+}
+
+function calcularDesconto(calcularTotal) {
+    const diaDaSemana = new Date().getDay();
+    if(diaDaSemana === 3) {
+        return calcularTotal * 0.9;
+    } else {
+        return calcularTotal
+    }
+}
+  
+
+function calcular() {
+    const bov = parseFloat(document.getElementById("bovina").value);
+    const suina = parseFloat(document.getElementById("suina").value);
+    const lingui = parseFloat(document.getElementById("linguiça").value);
+    const ave = parseFloat(document.getElementById("frango").value)
+
+    document.getElementById("total").innerHTML = "total: " + calcularTotal(bov, suina, lingui,ave);
+    document.getElementById("desconto").innerHTML = "valor com desconto: " + calcularDesconto(calcularTotal(bov, suina, lingui,ave));
+}
+
---
+++ atividade açougue/script2.js
@@ -0,0 +1,43 @@
+// Função para aplicar desconto de 10% nas quartas-feiras e 5% nas segundas-feiras
+function aplicarDesconto(valorTotal) {
+    const diaDaSemana = new Date().getDay(); // 0 para domingo, 1 para segunda-feira, etc.
+    if (diaDaSemana ===3) { //quarta feira
+        return valorTotal *0.9; //desconto de 10%
+    }
+    else if (diaDaSemana === 1) { //segunda feira
+        return valorTotal * 0.95;//desconto de 5%
+    }
+    else {
+        return valorTotal; //sem desconto
+    }
+}
+ 
+//tabela de preços
+const precoCarneBoi = 25.0;
+const precoCarneFrango = 15.0;
+const precoLiguica = 20.0;
+const precoCarnePorco = 30.0;
+
+//compras de João
+const kgCarneBoiJoão = 2;
+const kgCarneFrangoJoão = 1;
+const kgLingucaJoão = 1;
+const kgCarnePorcoJoão = 0;
+
+const totalJoao = (precoCarnePorco * kgCarnePorcoJoão) + (precoCarneBoi * kgCarneBoiJoão) + (precoCarneFrango * kgCarneFrangoJoão) + (precoLiguica * kgLingucaJoão);
+
+const totalJoaoComDesconto = aplicarDesconto(totalJoao);
+
+console.log("Total das compras de João", totalJoaoComDesconto.toFixed(2));
+
+//compras de Maria
+const kgCarneBoiMaria = 0;
+const kgCarneFrangoMaria = 2;
+const kgLingucaMaria = 1;
+const kgCarnePorcoMaria = 3;
+
+const totalMaria = (precoCarnePorco * kgCarnePorcoMaria) + (precoCarneBoi * kgCarneBoiMaria) + (precoCarneFrango * kgCarneFrangoMaria) + (precoLiguica * kgLingucaMaria);
+
+const totalMariaComDesconto = aplicarDesconto(totalMaria);
+
+console.log("Total das compras da Maria", totalMariaComDesconto.toFixed(2));
